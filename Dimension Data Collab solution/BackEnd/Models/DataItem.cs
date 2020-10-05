6a0edc8f21e8c0eb7c8ac6dd27fd39320f5e48f5
Demi147 <productionsDemigod@gmail.com>
2020-10-05 19:16:09 +0200

    Data connection update
    Successfully connected to a mongodb data base and added a test page to view the data and test connection speed.....

---
+++ Dimension Data Collab solution/BackEnd/Models/DataItem.cs
@@ -0,0 +1,49 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackEnd.Models
+{
+    public class DataItem
+    {
+        [BsonId]
+        public ObjectId _id;
+        public int Age { get; set; }
+        public bool Attrition { get; set; }
+        public string BusinessTravel { get; set; }
+        public double DailyRate { get; set; }
+        public string Department { get; set; }
+        public double DistanceFromHome { get; set; }
+        public int Education { get; set; }
+        public string EducationField { get; set; }
+        public int EmployeeCount { get; set; }
+        public int EmployeeNumber { get; set; }
+        public int EnvironmentSatisfaction { get; set; }
+        public string Gender { get; set; }
+        public double HourlyRate { get; set; }
+        public int JobInvolvement { get; set; }
+        public int JobLevel { get; set; }
+        public string JobRole { get; set; }
+        public int JobSatisfaction { get; set; }
+        public string MaritalStatus { get; set; }
+        public double MonthlyIncome { get; set; }
+        public double MonthlyRate { get; set; }
+        public int NumCompaniesWorked { get; set; }
+        public bool Over18 { get; set; }
+        public bool OverTime { get; set; }
+        public double PercentSalaryHike { get; set; }
+        public int PerformanceRating { get; set; }
+        public int RelationshipSatisfaction { get; set; }
+        public double StandardHours { get; set; }
+        public int StockOptionLevel { get; set; }
+        public double TotalWorkingYears { get; set; }
+        public int TrainingTimesLastYear { get; set; }
+        public int WorkLifeBalance { get; set; }
+        public int YearsAtCompany { get; set; }
+        public int YearsInCurrentRole { get; set; }
+        public int YearsSinceLastPromotion { get; set; }
+        public int YearsWithCurrManager { get; set; }
+    }
+}
---
+++ Dimension Data Collab solution/BackEnd/TestFolder/Helper.cs
@@ -0,0 +1,48 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace BackEnd.TestFolder
+{
+    public static class Helper
+    {
+        //mongo crud test
+        private static IMongoDatabase db;
+
+        public static object TestMetod<T>()
+        {
+            var client = new MongoClient("mongodb+srv://mainUser:BtPCEIb8e3Dbb06n@323projectdatacluster.k94r8.azure.mongodb.net/MainData?retryWrites=true&w=majority");
+            var db = client.GetDatabase("MainData");
+            var col = db.GetCollection<T>("Test");
+            return col;
+        }
+
+        public class TestModel{
+            [BsonId]
+            public Guid Id { get; set; }
+
+            public string Name { get; set; }
+
+            public string Random { get; set; }
+        }
+
+        public static void InsertTest()
+        {
+            var client = new MongoClient("mongodb+srv://mainUser:BtPCEIb8e3Dbb06n@323projectdatacluster.k94r8.azure.mongodb.net/MainData?retryWrites=true&w=majority");
+            var db = client.GetDatabase("MainData");
+            var col = db.GetCollection<TestModel>("ConnectionTest");
+            col.InsertOne(new TestModel { Name="Test",Random="Random value" });
+        }
+
+        public static List<T> GetData<T>()
+        {
+            var client = new MongoClient("mongodb+srv://mainUser:BtPCEIb8e3Dbb06n@323projectdatacluster.k94r8.azure.mongodb.net/MainData?retryWrites=true&w=majority");
+            var db = client.GetDatabase("MainData");
+            var col = db.GetCollection<T>("Test");
+            return col.Find(new BsonDocument()).ToList();
+        }
+
+    }
+}
