06bf75d29473abdcd12f75be0122a293136f09a5
Demi147 <productionsDemigod@gmail.com>
2020-11-05 15:38:10 +0200

    Data Dashboard Complete

--- Dimension Data Collab solution/BackEnd/BussinessLogic/DataLogic.cs
+++ Dimension Data Collab solution/BackEnd/BussinessLogic/DataLogic.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using MongoDB.Driver.Linq;
+using System.Linq;
 
 namespace BackEnd.BussinessLogic
 {
@@ -41,6 +42,96 @@ namespace BackEnd.BussinessLogic
 
             var temp = (x, y);
             return temp;
+        }
+
+        public async Task<(int[], double[], double[], double[], double[], double[])> GetComplexData() 
+        {
+            var rawData = await collection.Find(new BsonDocument()).ToListAsync();
+
+            int[] bins = { 18, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70,75 };
+
+            rawData = rawData.OrderBy(x => x.Age).ToList();
+            var labels = bins;
+
+
+
+            //jobinvolement
+            double[] jobinvolement = new double[bins.Length];
+            for (int i = 0; i < bins.Length-1; i++)
+            {
+                try
+                {
+                    jobinvolement[i] = rawData.Where(x => x.Age < bins[i + 1] && x.Age >= bins[i]).Select(x => x.JobInvolvement).Average();
+                }
+                catch (Exception)
+                {
+
+                }
+                
+            }
+
+            //JobSatisfaction
+            double[] JobSatisfaction = new double[bins.Length];
+            for (int i = 0; i < bins.Length - 1; i++)
+            {
+                try
+                {
+                    JobSatisfaction[i] = rawData.Where(x => x.Age < bins[i + 1] && x.Age >= bins[i]).Select(x => x.JobSatisfaction).Average();
+                }
+                catch (Exception)
+                {
+
+                }
+
+            }
+
+            //education
+            double[] education = new double[bins.Length];
+            for (int i = 0; i < bins.Length - 1; i++)
+            {
+                try
+                {
+                    education[i] = rawData.Where(x => x.Age < bins[i + 1] && x.Age >= bins[i]).Select(x => x.Education).Average();
+                }
+                catch (Exception)
+                {
+
+                }
+
+            }
+
+            //EnvironmentSatisfaction
+            double[] EnvironmentSatisfaction = new double[bins.Length];
+            for (int i = 0; i < bins.Length - 1; i++)
+            {
+                try
+                {
+                    EnvironmentSatisfaction[i] = rawData.Where(x => x.Age < bins[i + 1] && x.Age >= bins[i]).Select(x => x.EnvironmentSatisfaction).Average();
+                }
+                catch (Exception)
+                {
+
+                }
+
+            }
+
+            //JobLevel
+            double[] JobLevel = new double[bins.Length];
+            for (int i = 0; i < bins.Length - 1; i++)
+            {
+                try
+                {
+                    JobLevel[i] = rawData.Where(x => x.Age < bins[i + 1] && x.Age >= bins[i]).Select(x => x.JobLevel).Average();
+                }
+                catch (Exception)
+                {
+
+                }
+
+            }
+
+            //xvalues,jobinvolement,JobSatisfaction,education,EnvironmentSatisfaction,JobLevel
+            return (labels,jobinvolement,JobSatisfaction,education,EnvironmentSatisfaction,JobLevel);
         }
     }
 }
--- Dimension Data Collab solution/Dimension Data Collab/Controllers/DataAnalyticsController.cs
+++ Dimension Data Collab solution/Dimension Data Collab/Controllers/DataAnalyticsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using BackEnd.BussinessLogic;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver.Core.Operations;
+using Newtonsoft.Json;
 
 namespace Dimension_Data_Collab.Controllers
 {
@@ -24,7 +25,18 @@ namespace Dimension_Data_Collab.Controllers
         public async Task<IActionResult> Dashboard()
         {
             var data = await dataLogic.GetGenderCount();
-            ViewBag.Data = data;
+            ViewBag.MaleData = JsonConvert.SerializeObject(data.Item1);
+            ViewBag.FemaleData = JsonConvert.SerializeObject(data.Item2);
+
+            //complex data
+            var complexData = await dataLogic.GetComplexData();
+            //xvalues,jobinvolement,JobSatisfaction,education,EnvironmentSatisfaction,JobLevel
+            ViewBag.ComplexLabels = JsonConvert.SerializeObject(complexData.Item1);
+            ViewBag.jobinvolement = JsonConvert.SerializeObject(complexData.Item2);
+            ViewBag.JobSatisfaction = JsonConvert.SerializeObject(complexData.Item3);
+            ViewBag.education = JsonConvert.SerializeObject(complexData.Item4);
+            ViewBag.EnvironmentSatisfaction = JsonConvert.SerializeObject(complexData.Item5);
+            ViewBag.JobLevel = JsonConvert.SerializeObject(complexData.Item6);
             return View();
         }
     }
